c966ed970736f6a77b4878faaa723320802b1f2d
Maxim Slobodchikov <maxims@bu.edu>
2023-01-24 10:58:12 -0500

    First testing completed

--- assigns/00/Testing/testall.sh
+++ assigns/00/Testing/testall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sml < assign00-01-test.sml
-sml < assign00-02-test.sml
-sml < assign00-03-test.sml
-sml < assign00-04-test.sml
-sml < assign00-05-test.sml
+"C:\Program Files (x86)\SMLNJ\bin\sml.bat" < assign00-01-test.sml
+# sml < assign00-02-test.sml
+# sml < assign00-03-test.sml
+# sml < assign00-04-test.sml
+# sml < assign00-05-test.sml
 
 ###### end of [testall.sh] ######
